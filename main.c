dfdfa03fae5badc84f805f5b47c25eacd84fd1bf
Kelo <1376312818@qq.com>
2021-12-04 10:06:49 +0800

    Show help message when input wrong args

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void help() {
+  const char *help_str = 
+    "Usage:\n"
+    "\tvgg11_bn <weights_path> <image_path> <output_file>\n";
+  printf(help_str);
+}
+
+int main(int argc, char const *argv[]) {
+  if (argc != 4) {
+    help();
+  }
+  return 0;
+}
