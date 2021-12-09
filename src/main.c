2f05b49cdae0f0718ed7c06069f632649aa66534
p0ny <p0uy@outlook.com>
2021-12-09 20:05:55 +0800

    fix help msg

--- src/main.c
+++ src/main.c
@@ -5,7 +5,7 @@ void help() {
   const char *help_str = 
     "Usage:\n"
     "\tvgg11_bn <weights_path> <image_path> <output_file>\n";
-  printf(help_str);
+  printf("%s", help_str);
 }
 
 int main(int argc, char const *argv[]) {
