2a7e74856bbac2a0734f4cb9f800bb9625e5b42f
Kelo <1376312818@qq.com>
2021-12-09 20:08:04 +0800

    rename Matrix4D to Matrix

--- src/matrix.h
+++ src/matrix.h
@@ -7,36 +7,36 @@ typedef long double db;
 typedef struct {
     int a, b, c, d;
     db *v;
-} Matrix4D;
+} Matrix;
 
-int msize(Matrix4D *m) {
+int msize(Matrix *m) {
     return m->a * m->b * m->c * m->d;
 }
 
-int mpos(Matrix4D *m, int i, int j, int k, int l) {
+int mpos(Matrix *m, int i, int j, int k, int l) {
     return i * m->b * m->c * m->d + j * m->c * m->d + k * m-> d + l;
 }
 
-db mget(Matrix4D *m, int i, int j, int k, int l) {
+db mget(Matrix *m, int i, int j, int k, int l) {
     return m->v[mpos(m, i, j, k, l)];
 }
 
-void mcreate(Matrix4D *m) {
+void mcreate(Matrix *m) {
     m->v = (db*) malloc(sizeof(db) * m->a * m->b * m->c * m->d);
 }
 
-void mread(Matrix4D *m, FILE *fd) {
+void mread(Matrix *m, FILE *fd) {
     for (int i = 0; i < msize(m); ++i)
         fscanf(fd, "%Lf", &m->v[i]);
 }
 
-void ReLU(Matrix4D *input, Matrix4D *output) {
+void ReLU(Matrix *input, Matrix *output) {
     for (int i = 0; i < msize(input); ++i) {
         output->v[i] = input->v[i] > 0 ? input->v[i] : 0;
     }
 }
 
-void ReLUInplace(Matrix4D *input) {
+void ReLUInplace(Matrix *input) {
     for (int i = 0; i < msize(input); ++i) {
         input->v[i] = input->v[i] > 0 ? input->v[i] : 0;
     }
