77fafa6a353b8ddd79df41ee1fcfb4e43f47f946
Kelo <1376312818@qq.com>
2021-12-11 19:12:00 +0800

    transpose image

--- data/image.py
+++ data/image.py
@@ -7,5 +7,6 @@ if __name__ == "__main__":
   output_path = sys.argv[2]
   resized_image = Image.open(image_path).resize((224, 224))
   img_data = np.asarray(resized_image).astype("float32")
+  img_data = np.transpose(img_data, [2, 0, 1]);
   img_data = img_data.flatten()
   np.savetxt(output_path, img_data, newline=" ")
