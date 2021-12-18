17c38d1dee05f4b0fee6b183d7fc00b612e68bd4
Kelo <1376312818@qq.com>
2021-12-18 12:30:47 +0800

    update weights.py

--- data/weights.py
+++ data/weights.py
@@ -7,9 +7,10 @@ import sys
 if __name__ == "__main__":
   model = torch.hub.load('pytorch/vision:v0.10.0', 'vgg11_bn', pretrained=True)
   save_param = open(sys.argv[1], "w")
-  for name, param in model.named_parameters():
-    print(name, param.shape)
-    tmp_data = param.flatten().detach().numpy().astype(np.float64)
+  state_dict = model.state_dict()
+  for name in state_dict:
+    print(name, state_dict[name].shape)
+    tmp_data = state_dict[name].flatten().detach().numpy().astype(np.float32)
     for idx in range(tmp_data.__len__()):
       save_param.write(str(tmp_data[idx])+" ")
     save_param.write("\n")
